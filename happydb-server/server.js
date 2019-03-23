2388fbc40c1d944fe616c449a41864f3d94de4a6
Joshua Johnson <joshuaj1397@gmail.com>
2019-03-23 15:40:26 -0500

    Connected tedious to repo pattern

--- happydb-server/server.js
+++ happydb-server/server.js
@@ -1,16 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3005
+const express = require('express');
+const app = express();
+const port = 3005;
 
-const Connection = require('tedious').Connection;
+app.get('/', (req, res) => res.send('Hello World'));
 
-const config = {
-  server = process.env.server;
-  database = process.env.database;
-  dbUsername = process.env.dbUsername;
-  dbPassword = process.env.dbPassword;
-}
-
-app.get('/', (req, res) => res.send('Hello World'))
-
-app.listen(port, () => console.log(`I'll be right by your side till ${port}`))
+app.listen(port, () => console.log(`I'll be right by your side till ${port}`));
