b82944d2788cf7830dee79e2a79a7381461b6ed4
Joshua Johnson <joshuaj1397@gmail.com>
2019-03-26 12:08:21 -0500

    Removed ts

---
+++ happydb-server/Entities/Country.js
@@ -0,0 +1,21 @@
+export class Country {
+  constructor(
+    ID,
+    RegionID,
+    Name,
+    HappinessRank,
+    GDP,
+    Freedom,
+    GovTrust,
+    DystopiaResidual
+  ) {
+    this.ID = ID;
+    this.RegionID = RegionID;
+    this.Name = Name;
+    this.HappinessRank = HappinessRank;
+    this.GDP = GDP;
+    this.Freedom = Freedom;
+    this.GovTrust = GovTrust;
+    this.DystopiaResidual = DystopiaResidual;
+  }
+}
