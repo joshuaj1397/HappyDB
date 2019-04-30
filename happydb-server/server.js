31b3f0d5360e90f5993d2b2be9646e782e3a20c3
Joseph Wisor <joseph.wisor@gmail.com>
2019-04-30 01:08:21 -0500

    asnyc sql requests and more api endpoints

--- happydb-server/server.js
+++ happydb-server/server.js
@@ -29,49 +29,96 @@ app.get('/mutateData', (req, res) => { mutate.readCountries() });
 
 // get 2015data by country name
 app.get('/2015data/:countryName', function (req, res) {
-    row = execSql(`SELECT * FROM HappyData WHERE DataYear = 2015 AND countryId IN (SELECT ID FROM Country WHERE Name = '${req.params.countryName}')`);
-    res.send(row)
+    dataPromise = execSql(`SELECT * FROM HappyData WHERE DataYear = 2017 AND countryId IN (SELECT ID FROM Country WHERE Name = '${req.params.countryName}')`);
+    dataPromise.then(function (result) {
+        data = result;
+        console.log(data)
+        res.send(data)
+    }, function (err) {
+        console.log(err);
+    })
 })
 
 // get 2016data by country name
 app.get('/2016data/:countryName', function (req, res) {
-    row = execSql(`SELECT * FROM 2016data WHERE country='${req.params.countryName}'`);
-    res.send(row)
+    dataPromise = execSql(`SELECT * FROM HappyData WHERE DataYear = 2017 AND countryId IN (SELECT ID FROM Country WHERE Name = '${req.params.countryName}')`);
+    dataPromise.then(function (result) {
+        data = result;
+        console.log(data)
+        res.send(data)
+    }, function (err) {
+        console.log(err);
+    })
 })
 
 // get 2017data by country name
 app.get('/2017data/:countryName', function (req, res) {
-    row = execSql(`SELECT * FROM 2017data WHERE country='${req.params.countryName}'`);
-    res.send(row)
+    dataPromise = execSql(`SELECT * FROM HappyData WHERE DataYear = 2017 AND countryId IN (SELECT ID FROM Country WHERE Name = '${req.params.countryName}')`);
+    dataPromise.then(function (result) {
+        data = result;
+        console.log(data)
+        res.send(data)
+    }, function (err) {
+        console.log(err);
+    })
 })
+
+// get country by id
+app.get('/country/:countryId', function (req, res) {
+    dataPromise = execSql(`SELECT Name FROM Country WHERE ID = '${req.params.countryId}'`)
+    dataPromise.then(function (result) {
+        data = result;
+        console.log(data)
+        res.send(data)
+    }, function (err) {
+        console.log(err);
+    })
+})
+
+// get region by id
+app.get('/region/:regionId', function (req, res) {
+    dataPromise = execSql(`SELECT Name FROM Region WHERE ID = '${req.params.regionId}'`)
+    dataPromise.then(function (result) {
+        data = result;
+        console.log(data)
+        res.send(data)
+    }, function (err) {
+        console.log(err);
+    })
+})
+
 
 app.listen(port, () => console.log(`I'll be right by your side till ${port}`));
 
 // make a sql reauest
 function execSql(sqlString) {
-    console.log(sqlString)
-    var connection = new Connection(config);
-    var response;
-    var request = new Request(sqlString, function (err, rowCount, rows) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(rowCount + 'rows\n');
-            console.log(rows);
-            response = rows;
-        }
-    });
+    return new Promise(function (resolve, reject) {
+        console.log(sqlString)
+        var connection = new Connection(config);
+        var response;
+        var request = new Request(sqlString, function (err, rowCount, rows) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            } else {
+                console.log(rowCount + ' rows');
+                console.log(rows);
+                response = rows;
+            }
+        });
 
-    connection.on('connect', function (err) {
-        if (err) {
-            console.log(err)
-        } else {
-            connection.execSql(request);
-            request.on('done', function () {
-                connection.close();
-            })
-        }
+        connection.on('connect', function (err) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            } else {
+                connection.execSql(request);
+            }
+        })
+
+        request.on('done', function () {
+            connection.close();
+            resolve(response);
+        })
     })
-
-    return response;
 }
