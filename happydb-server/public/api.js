f019df0e922d81f3c63af741a4e23fe529757ecc
Joshua Johnson <joshuaj1397@gmail.com>
2019-05-02 01:45:27 -0500

    Added values to select fields

---
+++ happydb-server/public/api.js
@@ -0,0 +1,41 @@
+const HappyAPI = {
+  get2015DataByCountryName: function(countryName) {
+    fetch(baseUrl + '/2015Data/' + countryName)
+      .then((res) => res.json())
+      .then((result) => {
+        return result
+      })
+  },
+
+  get2016DataByCountryName: function(countryName) {
+    fetch(baseUrl + '/2016Data/' + countryName)
+      .then((res) => res.json())
+      .then((result) => {
+        return result
+      })
+  },
+
+  get2017DataByCountryName: function(countryName) {
+    fetch(baseUrl + '/2017Data/' + countryName)
+      .then((res) => res.json())
+      .then((result) => {
+        return result
+      })
+  },
+
+  getCountryById: function(id) {
+    fetch(baseUrl + '/country/' + id)
+      .then((res) => res.json())
+      .then((result) => {
+        return result
+      })
+  },
+
+  getRegionById: function(id) {
+    fetch(baseUrl + '/region/' + id)
+      .then((res) => res.json())
+      .then((result) => {
+        return result
+      })
+  },
+}
--- happydb-server/public/script.js
+++ happydb-server/public/script.js
@@ -1,54 +1,71 @@
 const baseUrl = 'http://localhost:3005'
 
-const HappyAPI = {
-  get2015DataByCountryName: function(countryName) {
-    fetch(baseUrl + '/2015Data/' + countryName)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          return result;
-        }
-      )
-  },
+// The svg
+var svg = d3.select('svg'),
+  width = +svg.attr('width'),
+  height = +svg.attr('height')
 
-  get2016DataByCountryName: function(countryName) {
-    fetch(baseUrl + '/2016Data/' + countryName)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          return result;
-        }
-      )
-  },
+// Map and projection
+var path = d3.geoPath()
+var projection = d3
+  .geoNaturalEarth()
+  .scale(width / 2 / Math.PI)
+  .translate([width / 2, height / 2])
+var path = d3.geoPath().projection(projection)
 
-  get2017DataByCountryName: function(countryName) {
-    fetch(baseUrl + '/2017Data/' + countryName)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          return result;
-        }
-      )
-  },
+// Data and color scale
+var data = d3.map()
+var colorScheme = d3.schemeGreens[6]
+colorScheme.unshift('#eee')
+var colorScale = d3
+  .scaleThreshold()
+  .domain([1, 6, 11, 26, 101, 1001])
+  .range(colorScheme)
 
-  getCountryById: function(id) {
-    fetch(baseUrl + '/country/' + id)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          return result;
-        }
-      )
-  },
+// Legend
+var g = svg
+  .append('g')
+  .attr('class', 'legendThreshold')
+  .attr('transform', 'translate(20,20)')
+g.append('text')
+  .attr('class', 'caption')
+  .attr('x', 0)
+  .attr('y', -6)
+  .text('Scale')
+var labels = ['0', '1-5', '6-10', '11-25', '26-100', '101-1000', '> 1000']
+var legend = d3
+  .legendColor()
+  .labels(function(d) {
+    return labels[d.i]
+  })
+  .shapePadding(4)
+  .scale(colorScale)
+svg.select('.legendThreshold').call(legend)
 
-  getRegionById: function(id) {
-    fetch(baseUrl + '/region/' + id)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          return result;
-        }
-      )
-  },
+// Load external data and boot
+d3.queue()
+  .defer(d3.json, 'http://enjalot.github.io/wwsd/data/world/world-110m.geojson')
+  .defer(d3.csv, 'mooc-countries.csv', function(d) {
+    data.set(d.code, +d.total)
+  })
+  .await(ready)
 
+function ready(error, topo) {
+  if (error) throw error
+
+  // Draw the map
+  svg
+    .append('g')
+    .attr('class', 'countries')
+    .selectAll('path')
+    .data(topo.features)
+    .enter()
+    .append('path')
+    .attr('fill', function(d) {
+      // Pull data for this country
+      d.total = data.get(d.id) || 0
+      // Set the color
+      return colorScale(d.total)
+    })
+    .attr('d', path)
 }
