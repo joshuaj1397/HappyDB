7c8d95cf6afc6a2bf84081b1cee35cba1871820c
Joshua Johnson <joshuaj1397@gmail.com>
2019-05-02 04:23:06 -0500

    Changed EOLs on CSVs to windows EOLs

--- INSERT_HAPPY_DATA.sql
+++ INSERT_HAPPY_DATA.sql
@@ -1,51 +1,51 @@
 USE Happy
 
 BULK INSERT dbo.Region
-    FROM 'C:\Users\xzoti\projects\HappyDB\Data\Region.csv'
-    WITH
-    (
+FROM 'C:/Users/xzoti/projects/HappyDB/Data/Region.csv'
+WITH
+(
 	CHECK_CONSTRAINTS,
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n'   --Use to shift the control to next row
-	)
+  ROWTERMINATOR = '\n'   --Use to shift the control to next row
+);
 
 BULK INSERT dbo.Country
-    FROM 'C:\Users\xzoti\projects\HappyDB\Data\Country.csv'
-    WITH
-    (
+FROM 'C:/Users/xzoti/projects/HappyDB/Data/Country.csv'
+WITH
+(
 	CHECK_CONSTRAINTS,
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n'   --Use to shift the control to next row
-	)
+  ROWTERMINATOR = '\n'   --Use to shift the control to next row
+);
 
 BULK INSERT dbo.HappyData
-    FROM 'C:\Users\xzoti\projects\HappyDB\Data\2015.csv'
-    WITH
-    (
+FROM 'C:/Users/xzoti/projects/HappyDB/Data/2015.csv'
+WITH
+(
 	CHECK_CONSTRAINTS,
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n'   --Use to shift the control to next row
-	)
+  ROWTERMINATOR = '\n'   --Use to shift the control to next row
+);
 
 BULK INSERT dbo.HappyData
-    FROM 'C:\Users\xzoti\projects\HappyDB\Data\2016.csv'
-    WITH
-    (
+FROM 'C:/Users/xzoti/projects/HappyDB/Data/2016.csv'
+WITH
+(
 	CHECK_CONSTRAINTS,
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n'   --Use to shift the control to next row
-	)
+  ROWTERMINATOR = '\n'   --Use to shift the control to next row
+);
 
 BULK INSERT dbo.HappyData
-    FROM 'C:\Users\xzoti\projects\HappyDB\Data\2017.csv'
-    WITH
-    (
+FROM 'C:/Users/xzoti/projects/HappyDB/Data/2017.csv'
+WITH
+(
 	CHECK_CONSTRAINTS,
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n'   --Use to shift the control to next row
-	)
+  ROWTERMINATOR = '\n'   --Use to shift the control to next row
+);
